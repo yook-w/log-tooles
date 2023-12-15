ef75c2f06518c4150b7d0179e93ee310e0d23a2a
yook-w <wywork888@163.com>
2023-12-15 16:41:49 +0800

    feat: 优化log输出

---
+++ README.md
@@ -0,0 +1,8 @@
+## log-tooles 是一个快速操作console.log的工具集
+
+>🍭 如果您喜欢，希望您可以点个好评
+
+#### 功能快捷键
+| 方法 | mac快捷键 |window快捷键|
+| --- | --------- |----------- |
+| 输出log | cmd+shift+L | ctrl+shift+L|
--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,10 @@ import * as vscode from 'vscode';
 import logSelectedText from './logSelectedText/index'
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('logSelectedText: ', logSelectedText);
   // 注册命令
-  let disposable = vscode.commands.registerCommand('extension.logSelectedText', logSelectedText);
+  let _logSelectedText = vscode.commands.registerCommand('extension.logSelectedText', logSelectedText);
   // 将命令添加到插件的上下文中
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(_logSelectedText);
 
 }
 
